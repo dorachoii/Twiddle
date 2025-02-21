3c02680c26a7ab1b621fb4a20e38341756e3cdbd
hellodoradora7@gmail.com <135581648+dorachoii@users.noreply.github.com>
2025-02-21 18:40:30 +0900

    Fix/ AVFoundation 관련 주석 추가 완료

--- Twiddle.swiftpm/FrameHandler.swift
+++ Twiddle.swiftpm/FrameHandler.swift
@@ -2,13 +2,15 @@ import AVFoundation
 import CoreImage
 
 class FrameHandler: NSObject, ObservableObject {
-    @Published var frame: CGImage?
+    @Published var frame: CGImage?    // videoOutput 담을 변수
     private var permissionGranted = true
+    
     private let captureSession = AVCaptureSession()
-    private let sessionQueue = DispatchQueue(label: "sessionQueue")
-    private let context = CIContext()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue")    // 백그라운드에서도 가능
     
+    private let context = CIContext()    // videoOutput 변환 과정에서 필요한 변수
     
+    // MARK: 가장 처음 실행할 것들
     override init() {
         super.init()
         self.checkPermission()
@@ -18,40 +20,38 @@ class FrameHandler: NSObject, ObservableObject {
         }
     }
     
+    // MARK: 권한 체크
     func checkPermission() {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized: // The user has previously granted access to the camera.
+        case .authorized: 
             self.permissionGranted = true
-            
-        case .notDetermined: // The user has not yet been asked for camera access.
+        case .notDetermined: 
             self.requestPermission()
-            
-            // Combine the two other cases into the default case
         default:
             self.permissionGranted = false
         }
     }
     
     func requestPermission() {
-        // Strong reference not a problem here but might become one in the future.
         AVCaptureDevice.requestAccess(for: .video) { [unowned self] granted in
             self.permissionGranted = granted
         }
     }
     
+    // MARK: captureSession - 카메라 장치 및 데이터 흐름 관리
     func setupCaptureSession() {
         let videoOutput = AVCaptureVideoDataOutput()
         
         guard permissionGranted else { return }
         
+        // MARK: Input - mac 전면 카메라
         guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,for: .video, position: .front) else { return }
-        //guard let videoDevice = AVCaptureDevice.default(.builtInTrueDepthCamera,for: .video, position: .front) else { return }
-        
-        
         guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
         guard captureSession.canAddInput(videoDeviceInput) else { return }
         captureSession.addInput(videoDeviceInput)
         
+        // MARK: Output - sampleBuffer에 받아서, avcapturevideodataoutput으로 변환해와야 한다.
+        // 그 대리자를 나로 설정하고 extension에서 동작 추가
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sampleBufferQueue"))
         captureSession.addOutput(videoOutput)
     }
@@ -59,6 +59,8 @@ class FrameHandler: NSObject, ObservableObject {
 
 
 extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    // MARK: View에 표시하기 위한 image 반환
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let cgImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
         
@@ -68,6 +70,7 @@ extension FrameHandler: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
     
+    // MARK: sample buffer에서 frame 읽어오기 위한 변환 과정
     private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> CGImage? {
         guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
         let ciImage = CIImage(cvPixelBuffer: imageBuffer)
